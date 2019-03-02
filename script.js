1b55e8c2ab1a1a3ca8e07569baf912bca8a87683
Svet <stsupryk@gmail.com>
2019-03-03 00:59:52 +0200

    add function that make teble

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
     // invocation
     burger();
+    getData();
 
     ///burger-menu
 
@@ -23,4 +24,43 @@
             menu.style.transform = ("translate(500%)");
         }
     }
+
+    //get data from JSON
+
+    function getData() {
+        let request = new XMLHttpRequest();
+        request.open('GET', 'get-dates.json', true);
+        request.send();
+        request.onload = function () {
+            if (request.status >= 200 && request.status < 400) {
+                let data = JSON.parse(request.responseText).data;
+                addinTable(Object.values(data));
+                console.log(Object.values(data));
+            } else {
+                throw new Error(`Something went wrong ${request.status}`);
+            }
+        };
+        request.onerror = function () {
+            alert("error"); // There was a connection error of some sort
+        };
+    }
+
+    // make table
+
+    function addinTable(dataArr) {
+
+        let table = document.querySelector('table');
+        let tbody = table.querySelector('tbody');
+        // let pageNum = document.querySelector('.pagenumber');
+
+        tbody.innerHTML = "";
+        for (let i = 0; i < 10; i++) {
+            tbody.insertAdjacentHTML("beforeend", `<tr><td>${i+1}</td><td> ${dataArr[i].name}</td><td><img src="${dataArr[i].image}"></td><td>${dataArr[i].country}</td><td>${dataArr[i].city}</td><td>${dataArr[i].date}</td></tr>`);
+        }
+        table.appendChild(tbody);
+        // pageNum.innerHTML = page + 1;
+    }
+
+
+
 })();
