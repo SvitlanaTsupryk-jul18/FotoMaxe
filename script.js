89fbce538357065bec3d82e2d4817c990ece89c8
Svet <stsupryk@gmail.com>
2019-03-03 02:59:28 +0200

    add script for button load more

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 (function () {
 
     // invocation
@@ -35,7 +37,8 @@
             if (request.status >= 200 && request.status < 400) {
                 let data = JSON.parse(request.responseText).data;
                 addinTable(Object.values(data));
-                console.log(Object.values(data));
+                loadMore(Object.values(data));
+                //console.log(Object.values(data));
             } else {
                 throw new Error(`Something went wrong ${request.status}`);
             }
@@ -47,20 +50,26 @@
 
     // make table
 
-    function addinTable(dataArr) {
-
+    function addinTable(dataArr, page = 0) {
         let table = document.querySelector('table');
         let tbody = table.querySelector('tbody');
-        // let pageNum = document.querySelector('.pagenumber');
 
-        tbody.innerHTML = "";
-        for (let i = 0; i < 10; i++) {
+        for (let i = 0 + page * 10; i < 10 + page * 10; i++) {
             tbody.insertAdjacentHTML("beforeend", `<tr><td>${i+1}</td><td> ${dataArr[i].name}</td><td><img src="${dataArr[i].image}"></td><td>${dataArr[i].country}</td><td>${dataArr[i].city}</td><td>${dataArr[i].date}</td></tr>`);
         }
         table.appendChild(tbody);
-        // pageNum.innerHTML = page + 1;
     }
 
+    //load more in table
 
+    function loadMore(dataArr) {
+        let buttonMore = document.querySelector(".btn");
+        let page = 1;
+        buttonMore.addEventListener("click", function () {
+            addinTable(dataArr, page);
+            page++;
+            console.log(page);
+        });
+    }
 
 })();
