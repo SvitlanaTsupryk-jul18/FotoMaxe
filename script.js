4deaef4e25b87d31899ad2e4f5d5524e1bbf2105
Svet <39680564+SvitlanaTsupryk-jul18@users.noreply.github.com>
2019-03-04 14:21:20 +0200

    delete preloader

--- script.js
+++ script.js
@@ -3,23 +3,11 @@
 (function () {
 
     // invocation
-    Preloader();
+   
     burger();
     getData();
 
-    //////preloader
-
-    function Preloader() {
-        document.body.onload = function () {
-            setTimeout(function () {
-                let preloader = document.querySelector(".preloader");
-                if (!preloader.classList.contains('done')) {
-                    preloader.classList.add('done');
-                }
-            }, 1000)
-        }
-    };
-    
+        
     ///burger-menu
 
     function burger() {
